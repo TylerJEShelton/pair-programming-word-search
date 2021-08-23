1bf3fffb6ad230b8052203d3371a68b1991538dd
Tyler <tylershelton99@gmail.com>
2021-08-23 01:14:04 +0000

    complete the Pair Programming - Word Search Challenge with Chioma

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is found horizontally, left to right", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,7 +31,56 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is found horizontally, right to left", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'U', 'L', 'A', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'VALUES');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is found vertically, top to bottom", function() {
+    const result = wordSearch([
+      ['A', 'W', 'A', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'R', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'I', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'E', 'L', 'A', 'V', 'R', 'G'],
+      ['W', 'H', 'L', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'L', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'E', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ARIELLE');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is found vertically, bottom to top", function() {
+    const result = wordSearch([
+      ['A', 'W', 'A', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'R', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'I', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'E', 'L', 'A', 'V', 'R', 'G'],
+      ['W', 'H', 'L', 'S', 'Y', 'C', 'R', 'L'],
+      ['B', 'F', 'A', 'E', 'N', 'I', 'Y', 'B'],
+      ['U', 'B', 'M', 'W', 'A', 'G', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'A', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'M', 'A', 'L'],
+    ], 'MAGIC');
+
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Worked with Chioma for the Pair Programming - Word Search Challenge
 
-module.exports = wordSearch
+const transpose = matrix => {
+  // transposes the matrix across the diagonal
+  return matrix[0].map((col, colIndex) => matrix.map((row, rowIndex) => matrix[rowIndex][colIndex]));
+};
+
+const wordSearch = (letters, word) => {
+  // check to see if the word is horizontal, left to right
+  let horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // check to see if the word is horizontal, right to left
+  horizontalJoin = letters.map(ls => ls.reverse().join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // transpose the array to check for vertical matches
+  const transposedLetters = transpose(letters);
+
+  // check to see if the word is vertical, top to bottom
+  let verticalJoin = transposedLetters.map(ls => ls.join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  // check to see if the word is vertical, bottom to top
+  verticalJoin = transposedLetters.map(ls => ls.reverse().join(''));
+  for (const l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
